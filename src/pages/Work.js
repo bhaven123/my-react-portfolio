7cdff44da8692d3e3fc8b170d89d3a64e7d4b8e2
bhaven123 <naikbhaven11@gmail.com>
2022-11-09 14:44:04 -0400

    Updated Project Image Tiles

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,46 +1,40 @@
 import classes from "./Work.module.css";
-import image from "../assets/Bhaven.jpg";
+import portfolio from "../assets/portfolio.png";
+import drImage from "../assets/dr.png";
+import ganImage from "../assets/gans.png";
 
 const Work = () => {
   return (
-    <div id="work" className={classes.block}>
-      <div className={classes.projects}>
-        <a
-          href="https://github.com/bhaven123/my-react-portfolio.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={image} alt="Portfolio" className={classes.image} />
-          <p className={classes.title}>Personal Portfolio</p>
-        </a>
-        <a
-          href="https://github.com/bhaven123/Project-GANs.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={image}
-            alt="GAN Demo"
-            className={classes.image}
-            width="300px"
-            height="400px"
-          />
-          <p className={classes.title}>
-            <span>GAN Augmentation</span>
-          </p>
-        </a>
+    <div id="work" className={classes.projects}>
+      <a
+        href="https://github.com/bhaven123/my-react-portfolio.git"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src={portfolio} alt="Portfolio" className={classes.image} />
+        <p className={classes.title}>Personal Portfolio</p>
+      </a>
+      <a
+        href="https://github.com/bhaven123/Project-GANs.git"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src={ganImage} alt="GAN Demo" className={classes.image} />
+        <p className={classes.title}>
+          <span>GAN Augmentation</span>
+        </p>
+      </a>
 
-        <a
-          href="https://github.com/bhaven123/Project-Diabetic-Retinopathy.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={image} alt="DR Demo UI" className={classes.image} />
-          <p className={classes.title}>
-            <span>Diabetic Retinopathy</span>
-          </p>
-        </a>
-      </div>
+      <a
+        href="https://github.com/bhaven123/Project-Diabetic-Retinopathy.git"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src={drImage} alt="DR Demo UI" className={classes.image} />
+        <p className={classes.title}>
+          <span>Diabetic Retinopathy</span>
+        </p>
+      </a>
     </div>
   );
 };
