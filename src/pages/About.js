46cef31c16c85fb7fe3a0d95ce19307a66d168a7
bhaven123 <naikbhaven11@gmail.com>
2022-11-09 18:59:12 -0400

    added content and styling

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,25 @@
 import classes from "./About.module.css";
+import profileImage from "../assets/profile.png";
 
 const About = () => {
   return (
     <div id="about" className={classes.position}>
-      <h1 className={classes.h1}>This is the About page.</h1>
+      <div className={classes.container}>
+        <img
+          src={profileImage}
+          alt="Profile Pic"
+          className={classes["profile-pic"]}
+        />
+        <h2 className={classes.h2}>Software Developer</h2>
+      </div>
+      <div>
+        <h1 className={classes.h1}>Hi! I'm Bhaven Naik</h1>
+        <p className={classes.about}>
+          Hi! I'm Bhaven Naik and I have an interest in creating Full-Stack
+          Applications. I am also interesed in Data Science and related
+          positions.
+        </p>
+      </div>
     </div>
   );
 };
