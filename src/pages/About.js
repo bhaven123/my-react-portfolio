f8c8bdd9fdd1fbff40e8c0bdca2f189d596a9a2b
bhaven123 <naikbhaven11@gmail.com>
2022-11-10 13:18:06 -0400

    added more content and styling

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,23 +1,52 @@
 import classes from "./About.module.css";
-import profileImage from "../assets/profile.png";
+import profileImage from "../assets/profile.jpg";
+import {
+  faReact,
+  faNode,
+  faHtml5,
+  faCss3,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const About = () => {
   return (
     <div id="about" className={classes.position}>
+      <h1 className={classes.heading}>
+        About <span>Me</span>
+      </h1>
       <div className={classes.container}>
+        <div className={classes.rounds}></div>
         <img
           src={profileImage}
           alt="Profile Pic"
           className={classes["profile-pic"]}
         />
-        <h2 className={classes.h2}>Software Developer</h2>
+        <div className={classes.rounds}></div>
       </div>
       <div>
-        <h1 className={classes.h1}>Hi! I'm Bhaven Naik</h1>
+        <h1 className={classes.greeting}>Hi! I'm Bhaven Naik</h1>
         <p className={classes.about}>
           Hi! I'm Bhaven Naik and I have an interest in creating Full-Stack
           Applications. I am also interesed in Data Science and related
           positions. I am looking to kickstart my professional career.
+        </p>
+        <br />
+        <p className={classes.about}>
+          This is the Tech Stack that I have been working on recently.
+          <ol>
+            <li>
+              ReactJS <FontAwesomeIcon icon={faReact} />
+            </li>
+            <li>
+              NodeJS <FontAwesomeIcon icon={faNode} />
+            </li>
+            <li>
+              HTML <FontAwesomeIcon icon={faHtml5} />
+            </li>
+            <li>
+              CSS <FontAwesomeIcon icon={faCss3} />
+            </li>
+          </ol>
         </p>
       </div>
     </div>
