bb4c956c85308e48de75336418929b6d0d24b69c
bhaven123 <naikbhaven11@gmail.com>
2022-11-17 12:33:25 -0400

    added loading spinner

--- src/App.js
+++ src/App.js
@@ -4,17 +4,35 @@ import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
 import Work from "./pages/Work";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <Fragment>
-      <Navbar />
-      <Home />
-      <Work />
-      <About />
-      <Contact />
-      <Footer />
+      {isLoading ? (
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      ) : (
+        <div>
+          <Navbar />
+          <Home />
+          <Work />
+          <About />
+          <Contact />
+          <Footer />
+        </div>
+      )}
     </Fragment>
   );
 };
