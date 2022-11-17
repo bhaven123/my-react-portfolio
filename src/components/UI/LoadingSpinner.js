e2aeea2ff51b4cde7ce72f223f710ff38856d7f7
bhaven123 <naikbhaven11@gmail.com>
2022-11-17 18:36:38 -0400

    added rainbow spinner

--- src/components/UI/LoadingSpinner.js
+++ src/components/UI/LoadingSpinner.js
@@ -1,7 +1,27 @@
 import classes from "./LoadingSpinner.module.css";
 
 const LoadingSpinner = () => {
-  return <div className={classes.spinner}></div>;
+  return (
+    <div class={classes.loader}>
+      <div class={classes["loader-inner"]}>
+        <div class={classes["loader-line-wrap"]}>
+          <div class={classes["loader-line"]}></div>
+        </div>
+        <div class={classes["loader-line-wrap"]}>
+          <div class={classes["loader-line"]}></div>
+        </div>
+        <div class={classes["loader-line-wrap"]}>
+          <div class={classes["loader-line"]}></div>
+        </div>
+        <div class={classes["loader-line-wrap"]}>
+          <div class={classes["loader-line"]}></div>
+        </div>
+        <div class={classes["loader-line-wrap"]}>
+          <div class={classes["loader-line"]}></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoadingSpinner;
