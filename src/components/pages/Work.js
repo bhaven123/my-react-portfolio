c5853b39fb5ed72ab799c07587d345edda6860d3
bhaven123 <naikbhaven11@gmail.com>
2023-02-07 15:44:39 -0400

    added more projects

--- src/components/pages/Work.js
+++ src/components/pages/Work.js
@@ -1,6 +1,8 @@
 import portfolio from "../../assets/portfolio.png";
 import drImage from "../../assets/dr.png";
 import ganImage from "../../assets/gans.png";
+import contactFormImage from "../../assets/contactform.png";
+import modelDeployImage from "../../assets/modeldemo.png";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faLink } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -15,6 +17,7 @@ import {
 
 const Work = () => {
   return (
+    /* Making use of React-Bootstrap for designing the Work Section */
     <Container id="work" className="min-vh-100 py-3">
       <h1
         className="w-25 mx-auto text-center border-3 border-bottom border-secondary border-end mb-5 pb-3 fs-1 fw-bolder fw-bolder"
@@ -30,6 +33,126 @@ const Work = () => {
           className="g-5 text-justify mx-auto"
           style={{ color: "#ccd6f6" }}
         >
+          <Col>
+            <Card border="light" bg="dark" style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={contactFormImage} />
+              <Card.Body>
+                <Card.Title>
+                  Full-Stack Contact Form{" "}
+                  <Card.Link
+                    href="https://github.com/bhaven123/contact-form-with-backend"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FontAwesomeIcon icon={faGithub} />
+                  </Card.Link>
+                </Card.Title>
+                <Card.Text>
+                  A Contact Form that can be integrated into any portfolio
+                  website.
+                </Card.Text>
+              </Card.Body>
+              <ListGroup className="list-group-flush">
+                <ListGroup.Item
+                  variant="dark"
+                  className="bg-dark"
+                  style={{ color: "#ccd6f6" }}
+                >
+                  ReactJS, ExpressJS, Material-UI, Formik, Yup
+                </ListGroup.Item>
+              </ListGroup>
+              <Card.Body>
+                <Accordion flush>
+                  <Accordion.Item eventKey="0">
+                    <Accordion.Header>Description</Accordion.Header>
+                    <Accordion.Body>
+                      <ListGroup>
+                        <ListGroup.Item
+                          className="bg-dark"
+                          style={{ color: "#ccd6f6" }}
+                        >
+                          Created frontend contact form using ReactJS and
+                          designed it using Material-UI.
+                        </ListGroup.Item>
+                        <ListGroup.Item
+                          className="bg-dark"
+                          style={{ color: "#ccd6f6" }}
+                        >
+                          Created a backend server using ExpressJS and used
+                          Nodemailer to send an email on form submission.
+                        </ListGroup.Item>
+                        <ListGroup.Item
+                          className="bg-dark"
+                          style={{ color: "#ccd6f6" }}
+                        >
+                          Used Formik to handle the form submission and yup for
+                          form object schema validation.
+                        </ListGroup.Item>
+                      </ListGroup>
+                    </Accordion.Body>
+                  </Accordion.Item>
+                </Accordion>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col>
+            <Card border="light" bg="dark" style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={modelDeployImage} />
+              <Card.Body>
+                <Card.Title>
+                  ML Model Deployment{" "}
+                  <Card.Link
+                    href="https://github.com/bhaven123/model-deploy-demo"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FontAwesomeIcon icon={faGithub} />
+                  </Card.Link>
+                </Card.Title>
+                <Card.Text></Card.Text>
+              </Card.Body>
+              <ListGroup className="list-group-flush">
+                <ListGroup.Item
+                  variant="dark"
+                  className="bg-dark"
+                  style={{ color: "#ccd6f6" }}
+                >
+                  Python, FastAPI, Docker, PyTorch
+                </ListGroup.Item>
+              </ListGroup>
+              <Card.Body>
+                <Accordion flush>
+                  <Accordion.Item eventKey="0">
+                    <Accordion.Header>Description</Accordion.Header>
+                    <Accordion.Body>
+                      <ListGroup>
+                        <ListGroup.Item
+                          className="bg-dark"
+                          style={{ color: "#ccd6f6" }}
+                        >
+                          Used Hugging Face Inference API for the ML model.
+                        </ListGroup.Item>
+                        <ListGroup.Item
+                          className="bg-dark"
+                          style={{ color: "#ccd6f6" }}
+                        >
+                          Created Model API using FastAPI.
+                        </ListGroup.Item>
+                        <ListGroup.Item
+                          className="bg-dark"
+                          style={{ color: "#ccd6f6" }}
+                        >
+                          Created Docker Image for deployment.
+                        </ListGroup.Item>
+                      </ListGroup>
+                    </Accordion.Body>
+                  </Accordion.Item>
+                </Accordion>
+              </Card.Body>
+            </Card>
+          </Col>
+
           <Col>
             <Card border="light" bg="dark" style={{ width: "18rem" }}>
               <Card.Img variant="top" src={portfolio} />
