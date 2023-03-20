b7d1ce5452b118cff5cc9f1e33f16b43d7173301
bhaven123 <naikbhaven11@gmail.com>
2023-03-20 00:19:21 -0400

    justified text

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -28,9 +28,15 @@ const About = () => {
             className="w-25 h-25 object-fit-cover mx-auto border img-thumbnail border-secondary rounded-circle z-1 mb-3"
           />
         </Container>
-        <Container className="mb-0 fs-5" style={{ color: "#ccd6f6" }}>
+        <Container
+          className="mb-0 fs-5"
+          style={{
+            color: "#ccd6f6",
+            textAlign: "justify",
+          }}
+        >
           <h1 className="text-center">Hi! I'm Bhaven Naik</h1>
-          <p className="text-justify">
+          <p>
             As a Full Stack Engineer and Machine Learning enthusiast, I bring a
             unique blend of skills and experience to any project. With expertise
             in front-end and back-end technologies such as ReactJS, NodeJS,
@@ -42,14 +48,14 @@ const About = () => {
             develop intelligent systems that can analyze data, make predictions,
             and automate processes.
           </p>
-          <p className="text-justify">
+          <p>
             Whether it's building a robust web application or developing a
             machine learning model, I am passionate about learning and
             delivering high-quality work that meets business requirements. If
             you're looking for a Full Stack Engineer with expertise in Machine
             Learning, feel free to connect with me.
           </p>
-          <p className="text-justify">
+          <p>
             I am very excited to kickstart my professional career. I am a self
             starter and a quick learner. Additionally, I am open to learning new
             things. Currently, I am working on building a Task Management App
@@ -57,7 +63,7 @@ const About = () => {
             ReactJS and backend using NodeJS and ExpressJS with database
             connection to MongoDB.
           </p>
-          <p className="text-justify">The Tech stack that I have been using:</p>
+          <p>The Tech stack that I have been using:</p>
           <ol>
             <li>
               Python <FontAwesomeIcon icon={faPython} />
